ba21ac170d97fbcbd83903e58cfd2126509d8056
Diego336s <sanabriadiego336@gmail.com>
2025-07-14 22:04:30 -0500

    controller

---
+++ controller/CarrosController.ts
@@ -0,0 +1,19 @@
+import { Context, RouterContext, z } from "../dependencies/dependencias.ts";
+
+
+
+export const getCarros = async (ctx: Context)=>{
+
+}
+
+export const postCarros = async (ctx: Context)=>{
+
+}
+
+export const putCarros = async (ctx: Context)=>{
+
+}
+
+export const deleteCarros = async (ctx: RouterContext<"/carros/:id">) =>{
+
+}
