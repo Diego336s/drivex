9f4e9fb4f164dc480cd182c282b4fbaca738ae5a
Diego336s <sanabriadiego336@gmail.com>
2025-07-14 22:38:11 -0500

    fff

--- controller/CarrosController.ts
+++ controller/CarrosController.ts
@@ -3,7 +3,7 @@ import { Context, RouterContext, z } from "../dependencies/dependencias.ts";
 
 
 export const getCarros = async (ctx: Context)=>{
-Diego perra
+
 }
 
 export const postCarros = async (ctx: Context)=>{
